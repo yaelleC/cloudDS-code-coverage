f46cac8d5a615aeebee8cce780c09df097ae937c
yaellec <yaelle.chaudy@grafana.com>
2022-02-24 21:49:30 +0100

    Added script FE/BE

---
+++ testCoverage.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+set -euxo
+COV_FILE=~/Documents/Projects/cloudDS-code-covrage/testcoverage.md
+COV_FILE_TMP=~/Documents/Projects/cloudDS-code-covrage/coverage.txt
+PLUGINS=~/Documents/Projects/plugins/
+
+write_to_cov()
+{
+    TO_WRITE=$1
+    echo $1 >> "${COV_FILE}"
+}
+
+write_be_coverage()
+{
+    REPO=$1
+    go test ./... -coverprofile cover.out 
+    COVERAGE=$(go tool cover -func cover.out | grep total: | awk '{print $NF}')
+    write_to_cov "${REPO} = ${COVERAGE}" 
+}
+
+write_fe_coverage()
+{
+    REPO=$1
+    COVERAGE=$(yarn test:coverage | grep 'All files' | awk '{print $10}')
+    write_to_cov "${REPO} = ${COVERAGE}%" 
+}
+write_fe_core_coverage()
+{
+    REPO=$1
+    CODE_PATH=$2
+    COVERAGE=$(grep ${CODE_PATH} "${COV_FILE_TMP}" | head -1 | awk '{print $9}')
+    write_to_cov "${REPO} = ${COVERAGE}%"
+}
+
+write_to_cov "## $(date)" 
+write_to_cov "### Frontend" 
+
+cd ~/Documents/Projects/grafana
+# git checkout main
+# git pull
+yarn test:coverage .> "${COV_FILE_TMP}"
+
+write_fe_core_coverage 'CloudWatch' public/app/plugins/datasource/cloudwatch
+write_fe_core_coverage 'Cloud Monitoring' public/app/plugins/datasource/cloud-monitoring
+write_fe_core_coverage 'Azure Monitor' public/app/plugins/datasource/grafana-azure-monitor-datasource
+
+cd $PLUGINS;
+for d in */ ; do
+    cd $d
+    # git checkout main
+    # git pull
+    write_fe_coverage $d
+    cd $PLUGINS
+done
+
+write_to_cov "### Backend" 
+
+cd ~/Documents/Projects/grafana/pkg/tsdb/cloudwatch/
+    write_be_coverage 'CloudWatch'
+cd ~/Documents/Projects/grafana/pkg/tsdb/cloudmonitoring/
+    write_be_coverage 'Cloud Monitoring'  
+cd ~/Documents/Projects/grafana/pkg/tsdb/azuremonitor/
+    write_be_coverage 'Azure Monitor' 
+
+
+cd $PLUGINS;
+for d in */ ; do
+    cd $d
+    # git checkout main
+    # git pull
+    write_be_coverage $d
+    cd $PLUGINS
+done
+
+
---
+++ testcoverage.md
@@ -0,0 +1,19 @@
+## Thu Feb 24 21:45:53 CET 2022
+### Frontend
+CloudWatch = 78.24%
+Cloud Monitoring = 49.52%
+Azure Monitor = 69.03%
+athena-datasource/ = 88.43%
+azure-data-explorer-datasource/ = 29.26%
+redshift-datasource/ = 85.59%
+timestream-datasource/ = 33.72%
+x-ray-datasource/ = 69.26%
+### Backend
+CloudWatch = 73.9%
+Cloud Monitoring = 55.4%
+Azure Monitor = 57.3%
+athena-datasource/ = 59.6%
+azure-data-explorer-datasource/ = 63.5%
+redshift-datasource/ = 48.6%
+timestream-datasource/ = 69.2%
+x-ray-datasource/ = 70.3%
